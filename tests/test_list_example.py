58f8bf986dec740b09859d2280a3e04647e908dd
Alexey Zagumennov <truepikvic@gmail.com>
2020-04-15 19:16:15 +1000

    Fixed according flake8

--- tests/test_list_example.py
+++ tests/test_list_example.py
@@ -1,6 +1,7 @@
 import unittest
 from algodspy.list_example import ListExample
 
+
 class TestListExample(unittest.TestCase):
     def setUp(self):
         self.data = [1, 2, 3]
@@ -8,6 +9,6 @@ class TestListExample(unittest.TestCase):
 
     def test_list_init(self):
         self.assertEqual(self.lst.data, self.data)
-    
+
     def test_list_repr(self):
         self.assertEqual(self.lst.__repr__(), self.data.__repr__())
