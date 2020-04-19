93b9eb30e47b676dd97de841a60a62b9f130f193
uselesspanda10 <61774955+uselesspanda10@users.noreply.github.com>
2020-04-19 18:09:07 +1000

    Структура данных словарь (#21)
    Решена задача #16

---
+++ tests/test_dictionary.py
@@ -0,0 +1,34 @@
+import unittest
+from algodspy.dictionary import Dictionary
+
+
+class TestDictionary(unittest.TestCase):
+    def setUp(self):
+        self.data = {}
+        self.dictionary = Dictionary()
+        
+    def test_init(self):
+        self.assertEqual(self.dictionary.data, self.data)
+        
+    def test_append(self):
+        self.dictionary.append('q', 1)
+        self.data['q'] = 1
+        self.assertEqual(self.dictionary.data, self.data)
+        
+    def test_value(self):
+        self.dictionary.append('q', 1)
+        self.data['q'] = 1
+        self.assertEqual(self.dictionary.value('q'), self.data['q'])
+        
+    def test_change(self):
+        self.dictionary.append('q', 1)
+        self.data['q'] = 1
+        self.dictionary.change('q', 5)
+        self.data['q'] = 5
+        self.assertEqual(self.dictionary.data, self.data)
+        
+    def test_size(self):
+        self.dictionary.append('q', 1)
+        self.data['q'] = 1
+        self.assertEqual(self.dictionary.size(), len(self.data))
+    
