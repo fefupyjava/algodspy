abc33811ec362efae031282310cbe4db4cc28be7
karimidrisov <karim.idrisow2016@gmail.com>
2020-04-21 11:40:38 +1000

    Fixed test_heap_sort

--- tests/test_heap_sort.py
+++ tests/test_heap_sort.py
@@ -4,21 +4,18 @@ from algodspy.heap_sort import HeapSort
 
 class TestHeapSort(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_heap_sort_up(self):
         data = [4, 10, 3, 5, 1]
-        self.sorted_heap_sort = HeapSort()
-        self.sorted_heap_sort = self.sorted_heap_sort.sort(data)
-        self.data = sorted(data)
-        self.assertEqual(self.sorted_heap_sort, self.data)
+        sorted_heap_sort = HeapSort()
+        sorted_heap_sort = sorted_heap_sort.sort(data)
+        data = sorted(data)
+        self.assertEqual(sorted_heap_sort, data)
 
     def test_heap_sort_down(self):
         data = [4, 10, 3, 5, 1]
-        self.sorted_heap_sort = HeapSort()
-        self.sorted_heap_sort = self.sorted_heap_sort.sort(data, True)
-        self.data = sorted(data, reverse=True)
-        self.assertEqual(self.sorted_heap_sort, self.data)
+        sorted_heap_sort = HeapSort()
+        sorted_heap_sort = sorted_heap_sort.sort(data, True)
+        data = sorted(data, reverse=True)
+        self.assertEqual(sorted_heap_sort, data)
     # возможно нужен тест на большое кол-во исходных данный
     # з-за проблем питона с глубиной рекурсии
