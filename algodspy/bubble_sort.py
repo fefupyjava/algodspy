5b963adfa4132cf2d5b69158946bd93b2ff1f335
EGER <shumaher0268@gmail.com>
2020-04-22 18:35:14 +1000

    commit

--- algodspy/bubble_sort.py
+++ algodspy/bubble_sort.py
@@ -15,8 +15,3 @@ class BubbleSort:
                     if data[j] < data[j + 1]:
                         data[j], data[j + 1] = data[j + 1], data[j]
         return data
-
-
-data = [2, 1, 3, 4, 5]
-BubbleSort.sort(1,data, reverse=False)
-print(data)
