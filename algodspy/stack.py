a7fda636808c90bf5b816032915575ee4b7b4741
zgudasha <zguryad@gmail.com>
2020-04-23 01:40:20 +1000

    Reset all files

--- algodspy/stack.py
+++ algodspy/stack.py
@@ -1,16 +1,16 @@
 class Stack:
 
-    def __init__(self, array=[]):
-        self.stack = array
+    def __init__(self, array=None):
+        self.data = [] if not array else array 
 
     def append(self, value):
-        return self.stack.append(value)
+        return self.data.append(value)
 
     def pop(self):
-        return self.stack.pop()
+        return self.data.pop() if self.data else None
 
     def __len__(self):
-        return len(self.stack)
+        return self.data.__len__()
 
-    def __str__(self):
-        return f'{self.stack}'
+    def __repr__(self):
+        return self.data.__repr__()
