0db9b5e62c6908086f9c1a9000f70ded00903c41
BipolarRaisins <irinaneki@mail.ru>
2020-04-24 20:16:41 +1000

    Исправила названия тестов

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -4,28 +4,28 @@ from algodspy.fibonacci import Fibonacci
 
 class TestQuickSort(unittest.TestCase):
 
-    def test_sort_list_naive(self):
+    def test_naive(self):
         n = 1
         fib = Fibonacci()
         result = fib.run(n, 'naive')
         test = 1
         self.assertEqual(result, test)
 
-    def test_sort_list_recursive(self):
+    def test_recursive(self):
         n = 2
         fib = Fibonacci()
         result = fib.run(n, 'recursive')
         test = 1
         self.assertEqual(result, test)
 
-    def test_sort_list_memoized(self):
+    def test_memoized(self):
         n = 3
         fib = Fibonacci()
         result = fib.run(n, 'memoized')
         test = 2
         self.assertEqual(result, test)
 
-    def test_sort_list_dinamic(self):
+    def test_dinamic(self):
         n = 4
         fib = Fibonacci()
         result = fib.run(n, 'dinamic')
