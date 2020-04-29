16eab7a8825fe5f669be0d9a3106b554ff2dd2fc
Виктория Бердегулова <61866057+brdglv@users.noreply.github.com>
2020-04-29 15:42:30 +1000

    commit 2

--- algodspy/bucket_sort.py
+++ algodspy/bucket_sort.py
@@ -1,29 +1,25 @@
-def insertionSort(b): 
-    for i in range(1, len(b)): 
-        up = b[i] 
-        j = i - 1
-        while j >=0 and b[j] > up:  
-            b[j + 1] = b[j] 
-            j -= 1
-        b[j + 1] = up      
-    return b      
-              
-def bucketSort(x): 
-    arr = [] 
-    slot_num = 10 
-    for i in range(slot_num): 
-        arr.append([]) 
-          
-    for j in x: 
-        index_b = int(slot_num * j)  
-        arr[index_b].append(j) 
-      
-    for i in range(slot_num): 
-        arr[i] = insertionSort(arr[i]) 
-          
-    k = 0
-    for i in range(slot_num): 
-        for j in range(len(arr[i])): 
-            x[k] = arr[i][j] 
-            k += 1
-    return x 
+class BucketSort:
+
+    def __init__(self):
+        self.data = {}
+
+    def bucket_sort(self, alist, reverse=False):
+        m = max(alist)
+        n = len(alist)
+        size = m/n
+    
+        buckets = [[] for _ in range(n)]
+        for i in range(n):
+            j = int(alist[i]/size)
+            if j != n:
+                buckets[j].append(alist[i])
+            else:
+                buckets[n - 1].append(alist[i])
+    
+        result = []
+        for i in range(n):
+            result = result + buckets[i]
+    
+        return result
+
+
