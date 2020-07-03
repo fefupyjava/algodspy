3cb64f7cd62dd1f06efd9aed832d6fb63d2e3bc5
Turumuru <58406669+Turumuru@users.noreply.github.com>
2020-07-03 21:57:08 +1000

    факториал (#61)

---
+++ tests/test_factorial.py
@@ -0,0 +1,19 @@
+import unittest
+from algodspy.factorial import Factorial
+
+
+class TestFactorial(unittest.TestCase):
+
+    def test_naive(self):
+        n = 3
+        fact = Factorial()
+        result = fact.run(n, 'naive')
+        test = 6
+        self.assertEqual(result, test)
+
+    def test_recursive(self):
+        n = 4
+        fact = Factorial()
+        result = fact.run(n, 'recursive')
+        test = 24
+        self.assertEqual(result, test)
